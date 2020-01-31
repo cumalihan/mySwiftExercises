4171db3ebca523ed0eeee28ca4ba03325b5a5cbd
c <cmhunlu@yahoo.com>
2020-01-31 12:23:47 +0300

    SwiftUI App

---
+++ TimerApp/TimerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerApp
+//
+//  Created by Cumali Han Ünlü on 31.01.2020.
+//  Copyright © 2020 Cumali Han Ünlü. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
